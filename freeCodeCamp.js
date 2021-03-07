e600835dcef469895f5f7bdacd8a6ad7701bd6ea
pooya2293 <77061069+pooya2293@users.noreply.github.com>
2021-03-07 21:07:14 +0330

    hard challeng of js level 83

--- freeCodeCamp.js
+++ freeCodeCamp.js
@@ -758,7 +758,7 @@ delete ourDog.bark;
 
 //--------------------------------------
 
-Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
+// Convert the switch statement into an object called lookup. Use it to look up val and assign the associated string to the result variable.
 -->
 // Setup
 function phoneticLookup(val) {
@@ -796,7 +796,7 @@ You should not use case, switch, or if statements:
 -->
 
 
-// Setup
+// Setup 
 function phoneticLookup(val) {
   var result = "";
 
@@ -818,9 +818,9 @@ phoneticLookup("charlie");
 
 //------------------------------------
 
-Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
+// Sometimes it is useful to check if the property of a given object exists or not. We can use the .hasOwnProperty(propname) method of objects to determine if that object has the given property name. .hasOwnProperty() returns true or false if the property is found or not.
 
-Example
+// Example:
 
 var myObj = {
   top: "hat",
@@ -891,6 +891,75 @@ ourStorage.desk.drawer; // "stapler"
 
 
 
+//After updateRecords(collection, 5439, "artist", "ABBA"), artist should be the string ABBA
+
+Passed
+//After updateRecords(collection, 5439, "tracks", "Take a Chance on Me"), tracks should have the string Take a Chance on Me as the last element.
+
+Passed
+//After updateRecords(collection, 2548, "artist", ""), artist should not be set
+
+Passed
+//After updateRecords(collection, 1245, "tracks", "Addicted to Love"), tracks should have the string Addicted to Love as the last element.
+
+Passed
+//After updateRecords(collection, 2468, "tracks", "Free"), tracks should have the string 1999 as the first element.
+
+Passed
+//After updateRecords(collection, 2548, "tracks", ""), tracks should not be set
+
+Passed
+//After updateRecords(collection, 1245, "albumTitle", "Riptide"), albumTitle should be the string Riptide
+
+// Setup
+var collection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
+  }
+};
+
+// Only change code below this line
+function updateRecords(object, id, prop, value) {
+  if (value=== '') delete object[id][prop];
+  else if (prop ==='tracks') {
+    object[id][prop] = object[id][prop] || [];
+    object[id][prop].push(value);
+  } else {
+    object[id][prop] = value;
+  } 
+  return object;
+}
+
+//another solution:
+
+function updateRecords(object, id, prop, value) {
+  if (prop !== 'tracks' && value !== "") {
+    object[id][prop] = value;
+  } else if (prop === "tracks" && object[id].hasOwnProperty("tracks") === false) {
+    object[id][prop] = [value];
+  } else if (prop === "tracks" && value !== "") {
+    object[id][prop].push(value);
+  } else if (value === "") {
+    delete object[id][prop];
+  }
+  return object;
+}
+
+//----------------------------------------
 
 
 
